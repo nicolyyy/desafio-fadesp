d87f5c4b877cd7f4984a2cf4cc272da3e4ee7707
Nicoly Menezes <nicolymenezees@gmail.com>
2023-10-21 17:47:14 -0300

    create project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TarefaListaComponentComponent } from './components/tarefa-lista-component/tarefa-lista-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarefaListaComponentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/tarefa-lista-component/tarefa-lista-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarefa-lista-component',
+  templateUrl: './tarefa-lista-component.component.html',
+  styleUrls: ['./tarefa-lista-component.component.css']
+})
+export class TarefaListaComponentComponent {
+
+}
